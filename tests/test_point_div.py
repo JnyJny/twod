b3dddd9e517fe6172b53e97350f190866b260921
JnyJny <erik.oshaughnessy@gmail>
2018-11-09 08:54:38 -0600

    broke div tests into true and floor varients

--- tests/test_point_div.py
+++ tests/test_point_div.py
@@ -3,14 +3,14 @@ import pytest
 from twod import Point
 
 
-def test_point_division_with_zero_points():
+def test_point_truediv_with_zero_points():
     p = Point()
     q = Point()
     with pytest.raises(ZeroDivisionError):
         r = p / q
 
 
-def test_point_division_with_nonzero_points():
+def test_point_truediv_with_nonzero_points():
     p = Point(1, 1)
     q = Point(1, 1)
 
@@ -18,25 +18,66 @@ def test_point_division_with_nonzero_points():
     assert r.x == 1 and r.y == 1
 
 
-def test_point_division_with_scalars():
+def test_point_truediv_with_scalars():
     p = Point(1, 1) / 1
     assert p.x == 1 and p.y == 1
 
 
-def test_point_inplace_division_with_zero_points():
+def test_point_inplace_truediv_with_zero_points():
     p = Point(1, 1)
     p /= Point(1, 1)
     assert p.x == 1 and p.y == 1
 
 
-def test_point_inplace_division_with_nonzero_points():
+def test_point_inplace_truediv_with_nonzero_points():
     p = Point(2, 4)
     p /= Point(2, 2)
     assert p.x == 1 and p.y == 2
 
 
-def test_point_inplace_division_with_scalars():
+def test_point_inplace_truediv_with_scalars():
 
     p = Point(2, 4)
     p /= 2
     assert p.x == 1 and p.y == 2
+
+
+
+
+def test_point_floordiv_with_zero_points():
+    p = Point()
+    q = Point()
+    with pytest.raises(ZeroDivisionError):
+        r = p // q
+
+
+def test_point_floordiv_with_nonzero_points():
+    p = Point(1, 1)
+    q = Point(1, 1)
+
+    r = p // q
+    assert r.x == 1 and r.y == 1
+
+
+def test_point_floordiv_with_scalars():
+    p = Point(1, 1) // 1
+    assert p.x == 1 and p.y == 1
+
+
+def test_point_inplace_floordiv_with_zero_points():
+    p = Point(1, 1)
+    p //= Point(1, 1)
+    assert p.x == 1 and p.y == 1
+
+
+def test_point_inplace_floordiv_with_nonzero_points():
+    p = Point(2, 4)
+    p //= Point(2, 2)
+    assert p.x == 1 and p.y == 2
+
+
+def test_point_inplace_floordiv_with_scalars():
+
+    p = Point(2, 4)
+    p //= 2
+    assert p.x == 1 and p.y == 2    
