a82ae92cbc94b230682ed8cad0a5b007bc0ebfe9
JnyJny <erik.oshaughnessy@gmail>
2018-11-09 10:43:33 -0600

    added docstring stubs to Point methods

--- twod/point.py
+++ twod/point.py
@@ -10,12 +10,18 @@ class Point:
 
     @property
     def is_origin(self):
+        '''
+        '''
         return self.x == 0 and self.y == 0
 
     def __iter__(self):
+        '''
+        '''
         return iter((self.x, self.y))
 
     def __add__(self, other):
+        '''
+        '''
         try:
             return Point(self.x+other.x, self.y+other.y)
         except AttributeError:
@@ -23,6 +29,8 @@ class Point:
         return Point(self.x+other, self.y+other)
 
     def __iadd__(self, other):
+        '''
+        '''
         try:
             self.x += other.x
             self.y += other.y
@@ -34,6 +42,8 @@ class Point:
         return self
 
     def __sub__(self, other):
+        '''
+        '''
         try:
             return Point(self.x-other.x, self.y-other.y)
         except AttributeError:
@@ -41,6 +51,8 @@ class Point:
         return Point(self.x-other, self.y-other)
 
     def __isub__(self, other):
+        '''
+        '''
         try:
             self.x -= other.x
             self.y -= other.y
@@ -52,6 +64,8 @@ class Point:
         return self
 
     def __mul__(self, other):
+        '''
+        '''
         try:
             return Point(self.x*other.x, self.y*other.y)
         except AttributeError:
@@ -59,6 +73,8 @@ class Point:
         return Point(self.x*other, self.y*other)
 
     def __imul__(self, other):
+        '''
+        '''
         try:
             self.x *= other.x
             self.y *= other.y
@@ -71,6 +87,8 @@ class Point:
         
 
     def __truediv__(self, other):
+        '''
+        '''
         try:
             return Point(self.x/other.x, self.y/other.y)
         except AttributeError:
@@ -78,6 +96,8 @@ class Point:
         return Point(self.x/other, self.y/other)
 
     def __itruediv__(self, other):
+        '''
+        '''
         try:
             self.x /= other.x
             self.y /= other.y
@@ -89,6 +109,8 @@ class Point:
         return self
 
     def __floordiv__(self, other):
+        '''
+        '''
         try:
             return Point(self.x//other.x, self.y//other.y)
         except AttributeError:
@@ -96,6 +118,8 @@ class Point:
         return Point(self.x//other, self.y//other)                
 
     def __ifloordiv__(self, other):
+        '''
+        '''
         try:
             self.x //= other.x
             self.y //= other.y
@@ -107,60 +131,76 @@ class Point:
         return self
 
     def __pow__(self, exponent):
-
+        '''
+        '''
         return Point(self.x**exponent, self.y**exponent)
 
     def __ipow__(self, exponent):
+        '''
+        '''
         self.x **= exponent
         self.y **= exponent
         return self
 
     def __abs__(self):
+        '''
+        '''
         return Point(abs(self.x), abs(self.y))
 
     def __neg__(self):
+        '''
+        '''
         return Point(-self.x, -self.y)
     
 
     def distance(self, other=None):
-
+        '''
+        '''
         return math.sqrt(self.distance_squared(other or Point()))
 
     def distance_squared(self, other=None):
+        '''
+        '''
         return sum((((other or Point()) - self) ** 2))
 
     def dot(self, other):
-        
+        '''
+        '''
         return sum(self * other)
 
     def cross(self, other):
-
+        '''
+        '''
         return (self.x * other.y) + (self.y * other.x)
 
     def ccw(self, b, c):
-         
+        '''
+        '''
         return ((b.x-self.x)*(c.y-self.y)) - ((c.x-self.x) * (b.y-self.y))
 
     def is_ccw(self, b, c):
-
+        '''
+        '''
         result = self.ccw(b, c)
         if result == 0:
             raise ColinearPoints(self, b, c)
         return result > 0
 
     def is_colinear(self, b, c):
-
-        return self.ccw(b,c) == 0
+        '''
+        '''
+        return self.ccw(b, c) == 0
 
     def midpoint(self, other=None):
-
+        '''
+        '''
         return (self + (other or Point()))/2
 
     def between(self, p, q):
-
+        '''
+        '''
         i = (p.x <= self.x <= q.x) or (q.x <= self.x <= p.x)
         j = (p.y <= self.y <= q.y) or (p.y <= self.y <= p.y)
-
         return i and j
              
 
