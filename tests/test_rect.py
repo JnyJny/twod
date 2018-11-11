068ef4ca0ef0eb7153b0e55fe278c9de678fd844
JnyJny <erik.oshaughnessy@gmail>
2018-11-11 14:10:44 -0600

    updated test_rect to just test creation

--- tests/test_rect.py
+++ tests/test_rect.py
@@ -3,32 +3,44 @@ import pytest
 from twod.point import Point
 from twod.rect import Rect
 
+def test_rect_creation_no_args():
+    r = Rect()
+    assert r.x == 0 and r.y == 0 and r.w == 0 and r.h == 0
 
-def test_point_in_rect():
-    bb = Rect(1, 1, 2, 2)
-    assert bb.center in bb
+def test_rect_creation_x_args():
+    r = Rect(1)
+    q = Rect(x=2)
+    assert r.x == 1 and r.y == 0 and r.w == 0 and r.h == 0
+    assert q.x == 2 and q.y == 0 and q.w == 0 and q.h == 0
+
+def test_rect_creation_y_arg():
+    r = Rect(y=1)
+    assert r.x == 0 and r.y == 1 and r.w == 0 and r.h == 0
+
+def test_rect_creation_xy_args():
+    r = Rect(1, 2)
+    assert r.x == 1 and r.y == 2 and r.w == 0 and r.h == 0
+    
+def test_rect_creation_wh_args():
+    r = Rect(w=1, h=2)
+    assert r.x == 0 and r.y == 0 and r.w == 1 and r.h == 2
+
+def test_rect_creation_all_args():
+    r = Rect(1, 2, 3, 4)
+    assert r.x == 1 and r.y == 2 and r.w == 3 and r.h == 4
+
+def test_rect_equality_empty():
+    r = Rect()
+    q = Rect()
+    assert r == q
+
+def test_rect_equality_nonempty():
+    r = Rect(1, 2, 3, 4)
+    q = Rect(1, 2, 3, 4)
+    assert r == q    
 
 
-def test_point_not_in_rect():
-    p = Point()
-    bb = Rect(1, 1, 2, 2)
-    assert p not in bb
 
 
-def test_rect_in_rect():
-    bb = Rect(1, 1, 1, 1)
-    qq = Rect(0, 0, 3, 3)
-    assert bb in qq
 
 
-def test_outer_rect_in_inner_rect():
-    bb = Rect(1, 1, 1, 1)
-    qq = Rect(0, 0, 3, 3)
-    assert qq in bb
-
-
-def test_rect_not_in_rect():
-    bb = Rect(0, 0, 1, 1)
-    qq = Rect(10, 10, 1, 1)
-    assert bb not in qq
-    assert qq not in bb
