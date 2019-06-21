fd48c5277ef31dfb28a6a66fb42870a7e1690796
JnyJny <erik.oshaughnessy@gmail>
2019-06-21 15:07:40 -0500

    initial commit of twod.constants

---
+++ twod/constants.py
@@ -0,0 +1,13 @@
+"""geometric constants for humans™
+"""
+
+from enum import IntEnum
+
+class Quadrant(IntEnum):
+    ORIGIN = -1
+    I = 0
+    II = 1
+    III = 2
+    IV = 3
+
+EPSILON_EXP_MINUS_1 = 15
