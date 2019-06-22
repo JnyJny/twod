84452b067e296e7ed43384ac70ba5dbb15bbce58
JnyJny <erik.oshaughnessy@gmail>
2019-06-22 12:58:11 -0500

    Updated twod.Rect with new properties and tests.
    Added tests for these new Rect properties:
    - sides
    - perimeter
    - area
    - center.setter
    
    Refactored some of the division functions that were raising an
    exception while handling an exception. Now they pass and raise
    the new exception to avoid those awkward "caught an exception
    while handling other exception" stacks which are hard to debug.
    
    Farmed the docstrings a little, needs more work.

--- tests/test_rect.py
+++ tests/test_rect.py
@@ -46,3 +46,64 @@ def test_rect_equality_nonempty():
     r = Rect(1, 2, 3, 4)
     q = Rect(1, 2, 3, 4)
     assert r == q
+
+
+def test_rect_vertex_properties():
+    r = Rect(1, 2, 3, 4)
+    expected = [(1, 2), (4, 2), (4, 6), (1, 6)]
+
+    assert tuple(r.A) == expected[0]
+    assert tuple(r.B) == expected[1]
+    assert tuple(r.C) == expected[2]
+    assert tuple(r.D) == expected[3]
+
+    for vertex, value in zip(r.vertices, expected):
+        assert tuple(vertex) == value
+
+
+def test_rect_sides():
+    expected = [10, 5, 10, 5]
+
+    assert Rect(w=10, h=5).sides == expected
+    assert Rect(w=5, h=10).sides == expected[::-1]
+
+
+def test_rect_perimeter():
+    assert Rect().perimeter == 0
+    assert Rect(w=1, h=1).perimeter == 4
+    assert Rect(1, 2, 3, 4).perimeter == 14
+
+
+def test_rect_area():
+    assert Rect().area == 0
+    assert Rect(w=1, h=1).area == 1
+    assert Rect(1, 2, 3, 4).area == 12
+
+
+def test_rect_center_getter():
+
+    assert Rect().center == Point()
+    assert Rect(w=2, h=2).center == Point(1, 1)
+    assert Rect(1, 2, 3, 4).center == Point(2.5, 4)
+
+
+def test_rect_center_setter():
+
+    r = Rect(0, 0, w=2, h=2)
+    q = Rect(0, 0, w=2, h=2)
+
+    assert r.center == q.center
+
+    new_center = (0, 0)
+
+    r.center = new_center
+
+    assert r.center != q.center and r.center == Point(*new_center)
+    assert r.A == Point(-1, -1)
+    assert r.B == Point(1, -1)
+    assert r.C == Point(1, 1)
+    assert r.D == Point(-1, 1)
+
+    assert r.perimeter == q.perimeter
+    assert r.area == q.area
+    assert r.sides == q.sides
--- twod/rect.py
+++ twod/rect.py
@@ -5,40 +5,97 @@ from dataclasses import astuple, dataclass
 
 from .point import Point
 
+from typing import Any
+from typing import Dict
+from typing import List
+
 
 @dataclass
 class Rect(Point):
+    """A rectangle specified by an origin at (x,y) and
+    dimensions (w,h).
+    """
+
     w: int = 0
     h: int = 0
 
     @property
-    def A(self):
+    def A(self) -> Point:
+        """Point at (x,y)."""
         return Point(self.x, self.y)
 
     @property
-    def B(self):
+    def B(self) -> Point:
+        """Point at (x+w, y)."""
         return Point(self.x + self.w, self.y)
 
     @property
-    def C(self):
-        return Point(self.x, self.y) + Point(self.w, self.h)
+    def C(self) -> Point:
+        """Point at (x+w, y+h)."""
+        return Point(self.x + self.w, self.y + self.h)
 
     @property
-    def D(self):
+    def D(self) -> Point:
+        """Point at (x, y+h)."""
         return Point(self.x, self.y + self.h)
 
     @property
-    def vertices(self):
+    def vertices(self) -> List[Point]:
+        """The points A, B, C, and D in a list.
+        """
         return [self.A, self.B, self.C, self.D]
 
     @property
-    def center(self):
+    def sides(self) -> List[float]:
+        """The lengths of each side: AB, BC, CD, and DA.
+        """
+        return [
+            max(abs(self.A - self.B)),
+            max(abs(self.B - self.C)),
+            max(abs(self.C - self.D)),
+            max(abs(self.D - self.A)),
+        ]
+
+    @property
+    def center(self) -> Point:
+        """Point at the center of the rectangle (midpoint of AC).
+        """
         return self.A.midpoint(self.C)
 
-    def __contains__(self, other):
+    @center.setter
+    def center(self, new_center):
+        self.x, self.y = Point(*new_center) - (Point(self.w, self.h) / 2)
 
+    @property
+    def perimeter(self) -> float:
+        """The distance around this rectangle.
+        """
+        return sum(self.sides)
+
+    @property
+    def area(self) -> float:
+        """The area of this rectangle.
+        """
+        return self.w * self.h
+
+    def __contains__(self, other) -> bool:
+        """If other is a twod.Point, returns True if the point is inside this
+        rectangle.
+
+        If other is a twod.Rect, returns True if any of other's vertices are
+        inside or any of the target rectangle's verices are inside other.
+
+        Otherwise returns False.
+
+        Raises TypeError if other is not a Point or Rect.
+
+        """
         if not isinstance(other, Rect):
-            return other.between(self.A, self.C)
+            try:
+                return other.between(self.A, self.C)
+            except AttributeError:
+                pass
+            raise TypeError(f"expected Point or Rect, received {type(other)}")
 
         for v in other.vertices:
             if v.between(self.A, self.C):
@@ -52,6 +109,8 @@ class Rect(Point):
 
     def __add__(self, other):
         """
+        :param Point|Rect other:
+        :return: Rect
         """
         x = self.x + other.x
         y = self.y + other.y
@@ -65,6 +124,8 @@ class Rect(Point):
 
     def __iadd__(self, other):
         """
+        :param Point|Rect other:
+        :return: Rect
         """
         self.x += other.x
         self.y += other.y
@@ -77,6 +138,8 @@ class Rect(Point):
 
     def __sub__(self, other):
         """
+        :param Point|Rect other:
+        :return: Rect
         """
         x = self.x - other.x
         y = self.y - other.y
@@ -90,6 +153,8 @@ class Rect(Point):
 
     def __isub__(self, other):
         """
+        :param Point|Rect other:
+        :return: Rect
         """
         self.x -= other.x
         self.y -= other.y
@@ -102,6 +167,8 @@ class Rect(Point):
 
     def __mul__(self, other):
         """
+        :param Point|Rect other:
+        :return: Rect
         """
         x = self.x * other.x
         y = self.y * other.y
@@ -114,6 +181,10 @@ class Rect(Point):
         return Rect(x, y, w, h)
 
     def __imul__(self, other):
+        """
+        :param Point|Rect other:
+        :return: Rect
+        """
         self.x *= other.x
         self.y *= other.y
         try:
@@ -124,6 +195,10 @@ class Rect(Point):
         return self
 
     def __truediv__(self, other):
+        """
+        :param Point|Rect other:
+        :return: Rect
+        """
         try:
             x = self.x / other.x
             y = self.y / other.y
@@ -135,9 +210,14 @@ class Rect(Point):
                 h = self.h
             return Rect(x, y, w, h)
         except ZeroDivisionError:
-            raise ZeroDivisionError(other)
+            pass
+        raise ZeroDivisionError(other)
 
     def __itruediv__(self, other):
+        """
+        :param Point|Rect other:
+        :return: Rect
+        """
         try:
             self.x /= other.x
             self.y /= other.y
@@ -148,9 +228,14 @@ class Rect(Point):
                 pass
             return self
         except ZeroDivisionError:
-            raise ZeroDivisionError(other)
+            pass
+        raise ZeroDivisionError(other)
 
     def __floordiv__(self, other):
+        """
+        :param Point|Rect other:
+        :return: Rect
+        """
         try:
             x = self.x // other.x
             y = self.y // other.y
@@ -162,9 +247,14 @@ class Rect(Point):
                 h = self.h
             return Rect(x, y, w, h)
         except ZeroDivisionError:
-            raise ZeroDivisionError(other)
+            pass
+        raise ZeroDivisionError(other)
 
     def __ifloordiv__(self, other):
+        """
+        :param Point|Rect other:
+        :return: Rect
+        """
         try:
             self.x //= other.x
             self.y //= other.y
@@ -175,4 +265,5 @@ class Rect(Point):
                 pass
             return self
         except ZeroDivisionError:
-            raise ZeroDivisionError(other)
+            pass
+        raise ZeroDivisionError(other)
